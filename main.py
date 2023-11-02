c9914cb96f296926ab8b422c074ebae6a9ebafe7
omri hachem <111324065+hachem-web@users.noreply.github.com>
2023-11-02 20:07:52 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,49 +1,61 @@
-# Import necessary modules
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, url_for, render_template
 import tensorflow as tf
 import numpy as np
+import cv2
 from io import BytesIO
 
-# Create a Flask app
 app = Flask(__name__)
 
-# Load the trained model
 MODEL_PATH = "stressDetecttor_2"
 model = tf.saved_model.load(MODEL_PATH)
 
-# Define class names (replace with your own)
 class_names = ['nostress', 'stress']
 
-# Define the route for the home page
+def extract_face(image):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    if len(faces) == 0:
+        return None
+    (x, y, w, h) = faces[0]
+    return image[y:y+h, x:x+w]
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    prediction_result = None
-
     if request.method == "POST":
-        # Get the uploaded image file
-        file = request.files["file"]
-
-        if file:
-            # Convert the FileStorage object to BytesIO
+        files = request.files.getlist("file")
+        results = []
+        for file in files:
             img_bytes = BytesIO(file.read())
+            file_bytes = np.frombuffer(img_bytes.getvalue(), dtype=np.uint8)
+            image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
 
-            # Load and preprocess the image
-            img = tf.keras.utils.load_img(img_bytes, target_size=(48, 48))
-            img_array = tf.keras.utils.img_to_array(img)
-            img_array = tf.expand_dims(img_array, 0)
+            face_image = extract_face(image)
 
-            # Make predictions using the model
-            predictions = model(img_array)
-            score = tf.nn.softmax(predictions[0])
+            if face_image is not None:
+                resized_face = cv2.resize(face_image, (48, 48))
+                img_array = tf.keras.preprocessing.image.img_to_array(resized_face)
+                img_array = tf.expand_dims(img_array, 0)
+                predictions = model(img_array)
+                score = tf.nn.softmax(predictions[0])
+                predicted_class = class_names[np.argmax(score)]
+                confidence_percentage = 100 * np.max(score)
+                result_str = f"{predicted_class}  {confidence_percentage:.2f}% "
+                results.append(result_str)
+            else:
+                results.append("verifier cette image ")
 
-            # Get the predicted class label and confidence
-            predicted_class = class_names[np.argmax(score)]
-            confidence_percentage = 100 * np.max(score)
+        # Remove any empty results from the list
+        results = [result for result in results if result]
 
-            # Create a prediction result string
-            prediction_result = f"This image most likely belongs to {predicted_class} with a {confidence_percentage:.2f}% confidence."
+        return redirect(url_for('accueil', results=results))
 
-    return render_template("index.html", prediction_result=prediction_result)
+    return render_template("index.html")
+
+@app.route('/accueil')
+def accueil():
+    results = request.args.getlist('results')
+    return render_template('accueil.html', results=results)
 
 if __name__ == "__main__":
     app.run(debug=True)
